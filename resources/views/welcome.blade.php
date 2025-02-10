6f000e9ebbd7f6abf0c2a8e79161b7400b525e75
Quentin <qmialon@groupeirfaformation.fr>
2025-02-10 10:15:55 +0100

    minor esthetical changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,28 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <x-role-list/>
-        <a href="/subscribe">s'inscrire</a>
+        <div class="max-w-2xl mx-auto mt-8 bg-white shadow-md rounded-md p-6">
+            @guest
+                <a href="{{ route('subscribe') }}"
+                class="text-blue-500 hover:text-blue-600 font-semibold transition duration-300">
+                    S'inscrire
+                </a>
+                <a href="{{ route('login') }}"
+                class="text-green-500 hover:text-green-600 font-semibold transition duration-300">
+                    Se connecter
+                </a>
+            @else
+                <a href="{{ route('logout') }}"
+                class="text-red-500 hover:text-red-600 font-semibold transition duration-300"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Se déconnecter
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                    @csrf
+                </form>
+            @endguest
+    </div>
+    <x-role-list/>
+    <x-user-request-list/>
     </body>
 </html>
