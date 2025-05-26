037c88e13f8a43db6e528738e3388baf17291549
Quentin <qmialon@groupeirfaformation.fr>
2025-05-26 15:34:56 +0200

    keuwa ?

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
     {
         return [
             'name'  => fake()->name(),
-            'email'=> fake()->unique()->safeEmail,
+            'email'=> fake()->unique()->safeEmail(),
             'password' => hash('sha256','password'), // password is 'password'
         ];
     }
