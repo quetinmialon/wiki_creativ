81e00a48d7b52d518ec3dd2964b85482209d7b66
Quentin <qmialon@groupeirfaformation.fr>
2025-06-02 10:16:26 +0200

    visual update of http error page

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,5 @@
+@extends('errors.minimal')
+
+@section('title', __('Accès nécessitant une authentification'))
+@section('code', '401')
+@section('message', __('Accès nécessitant une authentification'))
---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,5 @@
+@extends('errors.minimal')
+
+@section('title', __('Trop de requêtes'))
+@section('code', '429')
+@section('message', __('Trop de requêtes'))
